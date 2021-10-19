52ad07fe02115851e8a84b2e6073cbd5c5f92754
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-10-19 17:24:53 +0400

    Router methods to list, add, update, and delete contacts

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => res.json({msg : "Welcome to the Contact Manager API..
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/contacts', require('./routes/contact'));
 app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
