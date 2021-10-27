ebb41f8034974e1f6528ad69b1ba8a2ed20751fd
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-10-27 17:22:53 +0400

    Register json web token, once user created and sent the token to the user
    Create JSON Web Token
    1. Create payload with user id.
    2. add jwt secret in config/default.json
    3. call sign() method in jsonwebtoken with param - payload, jwtsecret, object of options - expiresIn, a callback function to send token to the user
    
    Every time, the process has been verified with Postman.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../models/User');
 
 // @route   POST api/users 
@@ -28,16 +30,42 @@ router.post(
       let user = await User.findOne({ email });
 
       if(user) {
-        return res.status(400).json({ msg: "User already exists"}); // status 400: bad request
+        // status 400: bad request
+        return res.status(400).json({ msg: "User already exists"});
       }
  
-      user = new User({ name, email, password }); // it doesn't saved in DB, just created an instance
+      // it doesn't saved in DB, just created an instance
+      user = new User({ name, email, password });
       
+      // hash version of the password
       const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt); // hash version of the password
+      user.password = await bcrypt.hash(password, salt);
+      
+      // save in DB
       await user.save();
 
-      res.send('user saved');
+      // Create JSON Web Token
+
+      // Payload with user id
+      const payLoad = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // sign in jwt with - payload, secret, expiresIn 1 hour, callback fn to send the token to the user
+      jwt.sign(
+        payLoad,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 3600
+        },
+        (err, token) => {
+          if(err) throw err;
+          res.json({ token });
+        }
+      );
+
     } catch (err) {
       console.error(err);
       res.status(500).send('Server error');
