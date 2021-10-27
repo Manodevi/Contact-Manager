14dacb3afa5544fbb10c9fc8a832d75d38f53985
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-10-27 10:42:24 +0400

    Pull out details from user registration details request and checking for email existence
    1. Pull out details from request body.
    2. Check for email existence using mongoose method findOne()
    3. If email exists => return 400 status with msg "User already exists"
    4. If email not exists => create user instance model

--- routes/users.js
+++ routes/users.js
@@ -16,12 +16,26 @@ router.post(
     check('password', 'Please enter a password with 6 or more characters')
       .isLength({min: 6})
   ],
-  (req, res) => {  // '/' same as api/users since using in its current file
+  async (req, res) => {  // '/' same as api/users since using in its current file
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
       return  res.status(400).json({errors: errors.array()});
     }
-    res.send('passed');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = User.findOne({ email });
+
+      if(user) {
+        return res.status(400).json({ msg: "User already exists"}); // status 400: bad request
+      }
+ 
+      user = new User({ name, email, password }); // it doesn't saved in DB, just created an instance
+    } catch (err) {
+
+    }
+    
 });
 
 module.exports = router;
