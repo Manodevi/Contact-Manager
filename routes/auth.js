74943120e2d3b0249b50c5febfb2a160ebe96ad7
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-10-27 18:54:51 +0400

    Authenticate Route
    1. Check the request - to make sure to get the email and password
    2. Check with the email in the DB - for user existence
    3. If user exists, compare body password with has password in DB.
    4. If it matches, create json web token and send it to the user

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../models/User');
 
 // @route   GET api/auth 
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {  // '/' same as api/auth since using in its current file
-  res.send('Get logged in user');
-});
+router.get('/');
 
 
 // @route   POST api/auth 
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {  // '/' same as api/auth since using in its current file
-  res.send('Log in user');
-});
+router.post(
+    '/',
+    [
+      check('email', 'Please include a valid email').isEmail(),
+      check('password', 'Password is required').exists()
+    ],
+    async (req, res) => {  // '/' same as api/auth since using in its current file
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()} );
+      }      
+
+      const { email, password } = req.body;
+
+      try {
+        let user = await User.findOne({ email });
+
+        // check for user presence
+        if(!user) {
+          return res.status(400).json({ msg: "Invalid Credentials"});
+        }
+
+        // compare plain password with hash password in DB
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+          return res.status(400).json({ msg: "Invalid Credentials"});
+        }
+
+        // Create Json Web Token
+
+        // Payload with user id
+        const payLoad = {
+          user: {
+            id: user.id
+          }
+        };
+
+        // sign in jwt with - payload, secret, expiresIn 1 hour, callback fn to send the token to the user
+        jwt.sign(
+          payLoad,
+          config.get('jwtSecret'),
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+      }
+    }
+  );
 
 module.exports = router;
